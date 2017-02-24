68ffb8731728fd4c99c234a9bbf58c412ca5e23c
sgaikwad <sgaikwad84@hotmail.com>
2017-02-24 04:39:30 -0500

    Added Project structure

---
+++ ProductsApp/App_Start/BundleConfig.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Optimization;
+
+namespace ProductsApp.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.IgnoreList.Clear();
+
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrapjs").Include(
+                "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/Content/bootstrapcss").Include("~/Content/Site.css",
+                "~/Content/bootstrap.min.css",
+                "~/Content/bootstrap-responsive.min.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/customjs").Include(
+           "~/Scripts/CustomScript.js"));
+
+        }
+    }
+}
